37c903c36a3808b7afff34bb8d5a3d990f809474
ShubH9604 <153529720+ShubH9604@users.noreply.github.com>
2024-08-01 01:34:45 +0530

    Add files via upload

---
+++ inverted_pyramid.java
@@ -0,0 +1,13 @@
+//inverted_pyramid
+public class inverted_pyramid{
+    public static void main(String args[]){
+        int n = 5;
+        
+        for (int i=n; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
